e517d2feb83d83c81a2e9282298c33733f30178a
gideonoludeyi <gooludeyi@gmail.com>
2023-02-24 17:37:23 -0500

    setup app navigation
    uses `go_router` package

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import 'screens/home.dart';
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/',
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen(title: "Feed");
+      },
+    ),
+  ],
+);
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Notivate',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(title: 'Feed'),
+      routerConfig: _router,
       debugShowCheckedModeBanner: true,
     );
   }
